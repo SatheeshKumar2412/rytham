e512e159ec773ce288345526ba0dbe7689b95b5c
SK Design <sk07055984@gmail.com>
2026-05-21 13:03:13 +0530

    fix: hardcode compileSdk, ndkVersion and targetSdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 
 android {
     namespace = "com.satheesh.rytham"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,9 +21,9 @@ android {
     defaultConfig {
         applicationId = "com.satheesh.rytham"
         minSdk = 21
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     splits {
